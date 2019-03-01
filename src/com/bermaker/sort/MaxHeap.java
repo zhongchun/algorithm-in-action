0b56cff4c6c65586ce90c6c9511326dd1dd265e6
zhongchun <zhongchunyu@gmail.com>
2019-03-01 11:23:26 +0800

    add some algorithms

--- src/com/bermaker/sort/MaxHeap.java
+++ src/com/bermaker/sort/MaxHeap.java
@@ -1,54 +1,93 @@
 package com.bermaker.sort;
 
+/**
+ * max-heapify: 维护最大堆性质的关键
+ * build-max-heap：从无序的输入数据数组中构造一个最大堆
+ * heapsort：对一个数组进行原址排序
+ */
 public class MaxHeap {
+
     int[] heap;
+    
     int heapsize;
 
-    // 初始化，传入数组
+    /**
+     * 初始化，传入数组
+     */
     public MaxHeap(int[] array) {
         this.heap = array;
         heapsize = heap.length;
     }
 
-    // 求节点i的父节点
+    /**
+     * 求节点i的父节点
+     *
+     * @param i
+     *
+     * @return
+     */
     public int heapParent(int i) {
         return (i - 1) / 2;
     }
 
-    // 求节点i的左子节点
+    /**
+     * 求节点i的左子节点
+     *
+     * @param i
+     *
+     * @return
+     */
     public int heapLeft(int i) {
         return 2 * i + 1;
     }
 
-    // 求节点i的右子节点
+    /**
+     * 求节点i的右子节点
+     *
+     * @param i
+     *
+     * @return
+     */
     public int heapRight(int i) {
         return 2 * i + 2;
     }
 
-    // 维护堆的性质,此时间复杂度为O(log i)
+    /**
+     * 维护堆的性质, 此时间复杂度为O(log i)
+     *
+     * @param i
+     */
     public void heapMaximize(int i) {
         int l = heapLeft(i);
         int r = heapRight(i);
         int maximum = i;
-        if (l < heapsize && heap[l] > heap[i])
+        if (l < heapsize && heap[l] > heap[i]) {
             maximum = l;
-        if (r < heapsize && heap[r] > heap[maximum])
+        }
+        if (r < heapsize && heap[r] > heap[maximum]) {
             maximum = r;
-        if (maximum == i || maximum >= heapsize)
+        }
+        if (maximum == i || maximum >= heapsize) {
             return;
+        }
         int tmp = heap[i];
         heap[i] = heap[maximum];
         heap[maximum] = tmp;
         heapMaximize(maximum);
     }
 
-    // 构造最大堆,必须从下往上构造
+    /**
+     * 构造最大堆, 必须从下往上构造
+     */
     public void buildMaxHeap() {
-        for (int i = heapsize / 2; i >= 0; i--)
+        for (int i = heapsize / 2; i >= 0; i--) {
             heapMaximize(i);
+        }
     }
 
-    // 堆排序算法实现
+    /**
+     * 堆排序算法实现
+     */
     public void heapSort() {
         for (int i = heapsize - 1; i >= 0; i--) {
             int tmp = heap[i];
@@ -75,7 +114,7 @@ public class MaxHeap {
     }
 
     public static void main(String[] args) {
-        int[] array = new int[] { 3, 8, 6, 14, 2, 1 };
+        int[] array = new int[] {3, 8, 6, 14, 2, 1};
         MaxHeap heap = new MaxHeap(array);
         System.out.println("执行最大堆化前堆的结构：");
         printHeapTree(heap.heap);
